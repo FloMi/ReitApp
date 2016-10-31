c015f60259eddec31b3292426ffafd934d11a2ae
Florian Mittlböck <florian.mittlboeck@web.de>
2016-10-31 11:29:42 +0100

    add OSM,Check for android version,Ask permission

--- app/src/main/java/koegelbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/MapActivity.java
+++ app/src/main/java/koegelbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/MapActivity.java
@@ -1,13 +1,65 @@
 package koegelbauer_mittlboeck_wiesinger.diplomarbeit.htlgrieskirchen.at.reitapp;
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapActivity extends Activity {
+
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermissions();
+        }
+
+        org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(17);
+        GeoPoint startPoint = new GeoPoint(48.0859,13.5646);
+        mapController.setCenter(startPoint);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String message = "osmdroid permissions:";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nLocation to show user location.";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        }
     }
 }
