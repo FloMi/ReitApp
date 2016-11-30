2a6083bf78f3e45c5aa401c1bad296dd47a20ed9
Gregor Koeglbauer <gkoeglbauer@gmail.com>
2016-11-30 10:51:55 +0100

    Coordinates

--- app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/Coordinate.java
+++ app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/Coordinate.java
@@ -5,12 +5,8 @@ package koeglbauer_mittlboeck_wiesinger.diplomarbeit.htlgrieskirchen.at.reitapp;
  */
 
 public class Coordinate {
-    public String referenceSystem;
     public String geoLength;
     public String geoWidth;
-    public String eastingUTM;
-    public String northingUTM;
-    public int zone;
 
     public Coordinate()
     {
@@ -18,11 +14,7 @@ public class Coordinate {
 
     public Coordinate(String referenceSystem, String geoLength, String geoWidth, String eastingUTM, String northingUTM, int zone)
     {
-        this.referenceSystem = referenceSystem;
         this.geoLength = geoLength;
         this.geoWidth = geoWidth;
-        this.eastingUTM = eastingUTM;
-        this.northingUTM = northingUTM;
-        this.zone = zone;
     }
 }
