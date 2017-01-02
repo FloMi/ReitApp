eb3143475619565e5024102fdb254cee9c94afa9
Simon <sime290498@yahoo.de>
2017-01-02 16:48:16 +0100

    main menu finish + activities added ++

--- app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/MainMenuActivity.java
+++ app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package koeglbauer_mittlboeck_wiesinger.diplomarbeit.htlgrieskirchen.at.reitapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+    }
+    public void onClickStatistic(View view){
+        Intent intent = new Intent(this, StatisticActivity.class);
+        startActivity(intent);
+    }
+    public void onClickSecurity(View view){
+        Intent intent = new Intent(this, SecurityActivity.class);
+        startActivity(intent);
+    }
+    public void onClickSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    public void onClickLogout(View view){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
