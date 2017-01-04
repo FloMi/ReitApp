a2a54da233f2b053c7da0816a6f4722748ac7d5d
Simon <sime290498@yahoo.de>
2017-01-04 13:55:09 +0100

    statistic examples +

--- app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/StatisticActivity.java
+++ app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/StatisticActivity.java
@@ -2,6 +2,7 @@ package koeglbauer_mittlboeck_wiesinger.diplomarbeit.htlgrieskirchen.at.reitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatisticActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class StatisticActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
+        InitStats();
+    }
+
+    private void InitStats() {
+        TextView touren = (TextView) findViewById(R.id.textViewStats1);
+        TextView kilom = (TextView) findViewById(R.id.textViewStats2);
+        TextView gast = (TextView) findViewById(R.id.textViewStats3);
+        TextView kult = (TextView) findViewById(R.id.textViewStats4);
+
+        //Todo Datenbankzugriff und Daten anzeigen
     }
 }
