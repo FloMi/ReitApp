7565bceff665850ddc4b33e4ec57856d839629cc
Simon <sime290498@yahoo.de>
2017-01-05 14:43:22 +0100

    touractivity list

--- app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/TourActivity.java
+++ app/src/main/java/koeglbauer_mittlboeck_wiesinger/diplomarbeit/htlgrieskirchen/at/reitapp/TourActivity.java
@@ -2,12 +2,45 @@ package koeglbauer_mittlboeck_wiesinger.diplomarbeit.htlgrieskirchen.at.reitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class TourActivity extends AppCompatActivity {
+
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    ListView list;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        InitTourList();
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object listItem = list.getItemAtPosition(position);
+                Toast.makeText(TourActivity.this, listItem.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void InitTourList() {
+        list = (ListView) findViewById(R.id.tourList);
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        list.setAdapter(adapter);
+        adapter.clear();
+
     }
 }
